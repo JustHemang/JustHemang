2ff5c8cddb8a8ac931f97dde3f1587d512d7b6e9
Just Hemang <justhemang@gmail.com>
2026-09-02 16:55:21 +0530

    fix: bump JS cache buster to resolve missing module exports

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ import {
   initHeroAnimation,
   initContactHover,
   initWorkFilters
-} from './modules/animations.js?v=3';
+} from './modules/animations.js?v=4';
 import { 
   initTimecode, 
   initFilmstrip, 
